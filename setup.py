1013df4de6910801d2d8760c61ff630930aab64d
vinit <vinitsiriya@yahoo.com>
2023-01-08 04:03:20 +0530

    Updating requirements.
    Signed-off-by: vinit <vinitsiriya@yahoo.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='cli_example',
     version='1.0.0',
-    packages=['cli_example'],
+    #packages=['cli_example'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pytest==6.1.2",
+    ],
     entry_points={
         'console_scripts': [
             'myapp = cli_example.main:main',
